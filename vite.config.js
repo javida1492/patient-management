68e18531e1e2ef5f5438acd864d8c3ca36957f4e
Andrew Javidfar <javida1492@gmail.com>
2025-04-01 18:25:16 -0400

    Update package.json

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: "/patient-management/",
+  base: "./patient-management/",
   plugins: [react()],
 })
